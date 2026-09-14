8fb7a5cbbd7c19ce5fa6fbd12bd7b852272bbdb2
shawonahmadsaddat <77286918+shawonahmadsaddat@users.noreply.github.com>
2026-09-14 10:06:11 +0600

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE='finacle-deposit-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{
-  const copy=x.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return x;
-}).catch(()=>caches.match('./index.html')))));
